853338e4c8e0a9be0340ff01809154c603681c52
Jose Ivan <jigonzalez930209@gmail.com>
2026-07-09 17:22:46 -0300

    refactor: Remove Sci Plot components and related files
    - Deleted the main Sci Plot module, including the PythonChart and SeriesBuilder classes, to streamline the codebase.
    - Removed associated cache files to clean up the project structure.

--- python/sci_plot/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Sci Plot Python Bindings
-A Python library to interact with and generate configurations for Sci Plot.
-"""
-
-__version__ = "0.1.0"
-
-from .core.chart import PythonChart
-from .core.series import SeriesBuilder
